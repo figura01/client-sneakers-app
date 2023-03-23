9d7b2cddfca8c3296cbc8776936c814dc2fad919
Tom Paganel <paganel.tom@gmail.com>
2023-03-23 11:55:41 +0100

    navbar and submenu ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,41 @@
 import { Link } from "react-router-dom";
+import SubMenu from "./SubMenu";
+import Container from "@mui/material/Container";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import ShoppingBasketOutlinedIcon from "@mui/icons-material/ShoppingBasketOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import reactLogo from "../assets/logoBlanc.svg";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to={`shop`}>Eshop</Link>
-      <Link to={`#`}>Blog</Link>
-
+      <Container>
+        <div className="navbar">
+          <img src={reactLogo} alt="logo-brand" width={55} height={65} />
+          <div>
+            <div className="subnav">
+              <div className="subnavbtn">
+                <Link to={`shop`} className="nav-link">
+                  Eshop
+                </Link>
+                <SubMenu />
+              </div>
+            </div>
+            <Link to={`#`} className="nav-link">
+              Blog
+            </Link>
+          </div>
+          <div className="nav-icons">
+            <SearchOutlinedIcon />
+            <ShoppingBasketOutlinedIcon />
+            <PersonOutlineOutlinedIcon />
+          </div>
+        </div>
+      </Container>
     </nav>
   );
 };
+
 
 export default Navbar;
